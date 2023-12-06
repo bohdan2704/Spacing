40902e4fca4a6cf04fdf8d85870897ccf7dc165e
Bohdan <bohdan2704@gmail.com>
2023-12-06 18:11:31 +0200

    changing

--- src/main/java/com/example/spacing/App.java
+++ src/main/java/com/example/spacing/App.java
@@ -1,10 +1,10 @@
-//package com.example.spacing;
-//
-//import javafx.application.Application;
-//
-//public class App {
-//
-//    public static void main(String[] args) {
-//        launch();
-//    }
-//}
+package com.example.spacing;
+
+import javafx.application.Application;
+
+public class App {
+
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
